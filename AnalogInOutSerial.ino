4242ab68f03064b17c40b588a08937995838fd9b
DaviVolcan <davi.volcan@me.com>
2018-08-08 14:10:56 -0300

    Update AnalogInOutSerial.ino
    adicionado a funçao map para a saida ser em %

--- AnalogInOutSerial.ino
+++ AnalogInOutSerial.ino
@@ -1,10 +1,11 @@
-const int HIH4000 = 13;
+const int HIH4000 = 13;  // Analog input pin that the potentiometer is attached to
 double media()
 {
   uint32_t somador = 0;
   for(int cont=  0; cont <= 200-1; cont++)
   {
-  somador+= analogRead(HIH4000);
+ 
+    somador+= analogRead(HIH4000);
   }
   somador = (somador/200);
   return somador;
@@ -14,6 +15,6 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("\n sensor = ");
-  Serial.print(media());
+  Serial.print("\n% ");
+  Serial.print(map(media(),840,4096,0.00,100.00));
 }
