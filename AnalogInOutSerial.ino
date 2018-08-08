e150f506dbba0716bd0fc39c1d9cf77b154fd962
DaviVolcan <davi.volcan@me.com>
2018-08-08 01:46:22 -0300

    Update AnalogInOutSerial.ino

--- AnalogInOutSerial.ino
+++ AnalogInOutSerial.ino
@@ -1,10 +1,10 @@
-const int analogInPin = 13;  // Analog input pin that the potentiometer is attached to
+const int HIH4000 = 13;
 double media()
 {
   uint32_t somador = 0;
   for(int cont=  0; cont <= 200-1; cont++)
   {
-  somador+= analogRead(analogInPin);
+  somador+= analogRead(HIH4000);
   }
   somador = (somador/200);
   return somador;
